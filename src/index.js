51f39d504300194b4b29f4bde23d80d005ccfeea
Anish <anishawasthi16@gmail.com>
2024-01-25 00:33:06 +0530

    custom api responses and errors

--- src/index.js
+++ src/index.js
@@ -1,26 +1,18 @@
 import 'dotenv/config'
-import express from "express"
+import { app, port } from './app.js';
 import connectDB from "./db/index.js";
 
 
-
-
-connectDB();
-
-// const app = express();
-// const port = process.env.PORT || 3000;
-// ;( async ()=>{
-//     try {
-//        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-//        app.on('error', (error)=>{
-//         console.log("ERROR :: EXPRESS_APP_DB :: ", error);
-//         throw error;
-//        })
-       
-//        app.listen(port, ()=>{
-//             console.log(`App is listening on port ${port}`)
-//        })
-//     } catch (error) {
-//         console.log("ERROR :: DB_CONNECTION :: ", error);
-//     }
-// } )()
+connectDB()
+.then(() => {
+    app.on('error', (error)=>{ // global error handler 
+        console.log("ERROR :: EXPRESS_APP_ON :: ", error);
+        throw error;
+    })
+    app.listen(port, () => {
+        console.log(`Server is running on port ${port}`);
+    })
+})
+.catch((err)=>{
+    console.log("ERROR :: MongoDB connection failed !! :: ", err);
+})

86fa593c110c3a06e069eb7a03941def5582feb5
Anish <anishawasthi16@gmail.com>
2024-03-10 17:44:20 +0530

    updated comment and like controllers

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -3,24 +3,65 @@ import {Comment} from "../models/comment.model.js"
 import {ApiError} from "../utils/ApiError.js"
 import {ApiResponse} from "../utils/ApiResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
+import { User } from "../models/user.model.js"
 
 const getVideoComments = asyncHandler(async (req, res) => {
     //TODO: get all comments for a video
     const {videoId} = req.params
     const {page = 1, limit = 10} = req.query
-
+    
 })
 
 const addComment = asyncHandler(async (req, res) => {
     // TODO: add a comment to a video
+    const {videoId} = req.params
+    const {content} = req.body
+    const user = await User.findById(req.user._id)
+
+    if (!user) throw new ApiError(404, "the user does not exist");
+    if (!videoId) throw new ApiError(404, "incorrect video id");
+    if(!content) throw new ApiError(404, "cant post empty comment")
+
+    const newComment = await Comment.create({
+        owner: user._id,
+        video: videoId,
+        content
+    })
+
+    return res
+    .status(201)
+    .json(new ApiResponse(201, newComment, "new comment added successfully"));
 })
 
 const updateComment = asyncHandler(async (req, res) => {
     // TODO: update a comment
+    const {commentId} = req.params;
+    const {updatedContent} = req.body
+
+    if(!commentId) throw new ApiError(404, "comment ID missing");
+
+    const updatedComment = await Comment.findByIdAndUpdate(
+        commentId,
+        { content: updatedContent },
+        { new: true }
+    );
+    if(!updatedComment) throw new ApiError(404, "Comment Not Found or id is invalid")
+
+    return res //sending the new Comment in response
+    .status(201)
+    .json(new ApiResponse(201, updatedComment, "comment updated successfully"))
 })
 
 const deleteComment = asyncHandler(async (req, res) => {
-    // TODO: delete a comment
+    const {commentId} = req.params;
+    if(!commentId) throw new ApiError(404, "comment ID missing");
+    const deletedComment = await Comment.findByIdAndDelete(commentId);
+
+    if (!deletedComment) throw new ApiError(404, "Comment Not Found or id is invalid");
+
+    return res
+    .status(201)
+    .json(new ApiResponse(201, {}, "comment deleted successfully"))
 })
 
 export {
--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -3,10 +3,31 @@ import {Like} from "../models/like.model.js"
 import {ApiError} from "../utils/ApiError.js"
 import {ApiResponse} from "../utils/ApiResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
+import { User } from "../models/user.model.js"
 
 const toggleVideoLike = asyncHandler(async (req, res) => {
+    
+    if(!req.user){
+        throw new ApiError(400, "you need to login to like ")
+    }
     const {videoId} = req.params
-    //TODO: toggle like on video
+    const user = await User.findById(req.user._id);
+    let like = await Like.find({likedBy: user._id, video: videoId});
+
+    if(!like){ //if the like does not exist
+        like = await Like.create({
+            user: user._id,
+            video: videoId
+        })
+        return res
+        .status(201)
+        .json(new ApiResponse(201, like, "Liked the video successfully"));
+    }
+    
+    await Like.deleteOne({likedBy: user._id, video: videoId})
+    return res
+    .status(201)
+    .json(new ApiResponse(201, {}, "Removed the like successfully"));
 })
 
 const toggleCommentLike = asyncHandler(async (req, res) => {
@@ -18,11 +39,39 @@ const toggleCommentLike = asyncHandler(async (req, res) => {
 const toggleTweetLike = asyncHandler(async (req, res) => {
     const {tweetId} = req.params
     //TODO: toggle like on tweet
-}
-)
+})
 
 const getLikedVideos = asyncHandler(async (req, res) => {
     //TODO: get all liked videos
+    if(!req.user){
+        throw new ApiError(400, "you need to login to like ")
+    }
+
+    const liekdVideos = await Like.aggregate([
+        {
+            $match: {
+                likedBy: mongoose.Types.ObjectId(req.user._id),
+                video: { $exists: true }
+            }
+        },
+        {
+            $lookup: {
+                from: "videos", 
+                localField: "video",
+                foreignField: "_id",
+                as: "videos"
+            }
+        },
+        {
+            $addFields: {
+                videos: { $arrayElemAt: ["$videos", 0] } 
+            }
+        }
+    ]);
+
+    return res
+    .status(201)
+    .json(new ApiResponse(201, liekdVideos, "Removed the like successfully"));
 })
 
 export {
